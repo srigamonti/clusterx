e0341d303df7fe64bd5ea278d19af0076ff5386b
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2018-03-10 01:40:15 +0100

    add cluster class

---
+++ clusterx/clusters/cluster.py
@@ -0,0 +1,56 @@
+from collections import Counter
+
+class Cluster():
+    def __new__(cls, atom_indexes, atom_numbers):
+        for iai, ai in enumerate(atom_indexes):
+            for _iai, _ai in enumerate(atom_indexes):
+                if ai == _ai and atom_numbers[iai] != atom_numbers[_iai]:
+                    raise ValueError("Cluster may not have different species on the same site.")
+
+        if len(atom_indexes) != len(atom_numbers):
+            raise ValueError("Initialization error, number of sites in cluster different from number of species.")
+                    
+        cl = super(Cluster,cls).__new__(cls)
+        cl.__init__(atom_indexes, atom_numbers)
+        return cl
+    
+    def __init__(self, atom_indexes, atom_numbers):
+        self.ais = atom_indexes
+        self.ans = atom_numbers
+        self.npoints = len(self.ais)
+        
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            ais = self.ais
+            ans = self.ans
+            oais = other.ais
+            oans = other.ans
+            npoints = self.npoints
+            
+            if len(ais) != len(oais):
+                return False
+
+            if Counter(ais) != Counter(oais):
+                return False
+            
+            for i in range(npoints):
+                for j in range(npoints):
+                    if ais[i] == oais[j] and ans[i] != oans[j]:
+                        return False
+
+            return True
+        
+        else:
+            return False
+    
+    def get_idxs(self):
+        return self.ais
+    
+    def get_nrs(self):
+        return self.ans
+    
+    def set_idxs(self, atom_indexes):
+        self.ais = atom_indexes
+    
+    def set_nrs(self, atom_numbers):
+        self.ans = atom_numbers
---
+++ clusterx/test/test_cluster.py
@@ -0,0 +1,78 @@
+from clusterx.clusters.cluster import Cluster
+
+def test_cluster():
+    """
+    Test cluster class
+    """
+    try:
+        c1 = Cluster([0,1,2,1],[13,14,15,13])
+        is_multiple_species_in_site = False
+    except:
+        is_multiple_species_in_site = True
+
+    try:
+        c1 = Cluster([0,1,2,1],[13,14,15])
+        is_consistent_length = False
+    except:
+        is_consistent_length = True
+
+    try:
+        c1 = Cluster([],[])
+        can_create_empty = True
+    except:
+        can_create_empty = False
+
+        
+    c1 = Cluster([0,1,2,3],[13,14,15,16])
+    c2 = Cluster([0,2,1,3],[13,15,14,16])
+    eq1 = c1==c2
+
+    c1 = Cluster([0,1,2,3],[13,14,15,16])
+    c2 = Cluster([0,2,1,3],[13,14,15,16])
+    eq2 = c1!=c2
+    
+    c1 = Cluster([0,1,2,3],[13,14,14,16])
+    c2 = Cluster([0,2,1,3],[13,14,14,16])
+    eq3 = c1==c2
+    
+    c1 = Cluster([],[])
+    c2 = Cluster([0,2,1,3],[13,14,14,16])
+    eq4 = c1!=c2
+    
+    c1 = Cluster([0,1,2,1],[13,14,15,14])
+    c2 = Cluster([1,1,0,2],[14,14,13,15])
+    eq5 = c1==c2
+    
+    c1 = Cluster([0,1,2],[13,14,15])
+    c2 = Cluster([0,1,2,3],[13,14,15,16])
+    eq6 = c1!=c2
+
+    c3 = Cluster([0,1,2],[13,14,15]) 
+    c4 = Cluster([0,1,3],[13,14,15]) 
+    clusters = [c1,c2]
+    in1 = c3 in clusters
+    in2 = c4 not in clusters
+    print("end test inclusion")
+    
+    print ("\n\n========Test writes========")
+    print (test_cluster.__doc__)
+    print ("===========================\n")
+
+    print ("========Asserts========")
+
+    print("test is_multiple_species_in_site: ",is_multiple_species_in_site)
+    assert is_multiple_species_in_site
+    print("test is_consistent_length: ",is_consistent_length)
+    assert is_consistent_length
+    print("test can_create_empty: ",can_create_empty)
+    assert can_create_empty
+    print("test equalities:", eq1,eq2,eq3,eq4,eq5,eq6)
+    assert eq1
+    assert eq2
+    assert eq3
+    assert eq4
+    assert eq5
+    assert eq6
+    print("test array inclusion:", in1,in2)
+    assert in1
+    assert in2
