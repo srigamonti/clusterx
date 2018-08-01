1c847b3f8ec777d0c27ba2c59f000841726c9a72
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2018-08-01 13:55:20 +0200

    clusters selector

--- clusterx/clusters_selector.py
+++ clusterx/clusters_selector.py
@@ -2,11 +2,34 @@
 
 
 class ClustersSelector():
-    def __init__(self,method,**kwargs):
+    def __init__(self, method, clusters_pool, **kwargs):
         self.method = method
-        self.alpha = kwargs.pop("alpha",0.0)
-        self._fitter = None
-        self.update()
+        self.alpha0 = kwargs.pop("alpha0",0.0)
+        self.alpha1 = kwargs.pop("alpha1",10.0)
+        self.alpha_step = kwargs.pop("alpha_step",1.0)
+        self.fitter_size = kwargs.pop("fitter_size","linreg")
+
+        self.cpool = clusters_pool
+
+        self.ecis = []
+        self.rmse = []
+        self.cvs = []
+        self.set_sizes = []
+
+        self.optimal_clusters = None
+
+        self.lasso_sparsity = []
+
+        self.fitter_cv = None
+
+        #self.update()
+
+    def get_ecis(self):
+        return self.ecis
+
+    def get_rmse(self):
+        return self.rmse
+
 
     def select_clusters(self, x, p):
         """Select clusters
@@ -16,7 +39,7 @@ class ClustersSelector():
 
         ..math::
             xJ = p^T
-        
+
         where J are the effective cluster interactions.
 
         Parameters:
@@ -27,17 +50,49 @@ class ClustersSelector():
             Property values for the training structures set.
         """
         from sklearn.model_selection import LeaveOneOut
-        
-        if self.method == "lr_loocv":
-            if np.shape(p)[0] != np.shape(x)[0]:
-                print "Error(cross_validation.cv): Number of property values differs from number of rows in correlation matrix."
-                sys.exit(0)
 
-                
+        #if np.shape(p)[0] != np.shape(x)[0]:
+        #    print "Error(cross_validation.cv): Number of property values differs from number of rows in correlation matrix."
+        #    sys.exit(0)
 
-        X = [1, 2, 3, 4]
-        loo = LeaveOneOut()
-        for train, test in loo.split(X):
-            print("%s %s" % (train, test))
-            
+        if self.method == "size":
+            oc = self._select_clusters_increasing_size(x, p)
+            self.optimal_clusters = self.cpool.get_subpool(oc)
+            return self.optimal_clusters
 
+        if self.method == "lasso":
+            self._select_clusters_lasso(x, p)
+
+
+    def _select_clusters_increasing_size(self,x,p):
+        from sklearn.model_selection import LeaveOneOut
+        from sklearn.model_selection import cross_val_score
+        from sklearn import linear_model
+        from sklearn.metrics import make_scorer, r2_score, mean_squared_error
+
+        clsets = cpool.get_clusters_sets(grouping_strategy = "size")
+
+
+        if self.fitter_size == "linreg":
+            self.fitter_cv = linear_model.LinearRegression(fit_intercept=True, normalize=False)
+
+        #fitter_cv = linear_model.LinearRegression(fit_intercept=False, normalize=False)
+
+        #cv = []
+        rmse = []
+        rows = np.arange(len(energies))
+        ecis = []
+        ranks = []
+        sizes = []
+        for iset, clset in enumerate(clsets):
+            _comat = comat[np.ix_(rows,clset)]
+            _cvs = cross_val_score(self.fitter_cv, _comat, energies, cv=LeaveOneOut(), scoring = 'neg_mean_squared_error')
+            self.cvs.append(np.sqrt(-np.mean(_cvs)))
+            fitter_cv.fit(_comat,energies)
+            self.rmse.append(np.sqrt(mean_squared_error(fitter_cv.predict(_comat),energies)))
+            ecis.append(fitter_cv.coef_)
+            ranks.append(np.linalg.matrix_rank(_comat))
+            sizes.append(len(clset))
+
+    def _select_clusters_lasso(self,x,p):
+        pass
---
+++ clusterx/test/test_clusters_selector.py
@@ -0,0 +1,99 @@
+import clusterx as c
+from clusterx.parent_lattice import ParentLattice
+from clusterx.super_cell import SuperCell
+from clusterx.structure import Structure
+from clusterx.structures_set import StructuresSet
+from clusterx.clusters.clusters_pool import ClustersPool
+from clusterx.correlations import CorrelationsCalculator
+#from clusterx.model import ModelConstructor
+from clusterx.fitter import Fitter
+from ase import Atoms
+import numpy as np
+from clusterx.calculators.emt import EMT2
+from clusterx.utils import isclose
+from clusterx.clusters_selector import ClustersSelector
+
+def test_clusters_selector():
+    """Test model optimization
+
+    After successful execution of the test, the generated structures and
+    clusters pool may be visualized with the command::
+
+        ase gui test_clusters_selector_[...].json
+
+    """
+
+    cell = [[3,0,0],
+            [0,1,0],
+            [0,0,5]]
+    positions = [
+        [0,0,0],
+        [1,0,0],
+        [2,0,0]]
+    pbc = [True,True,False]
+
+    pri = Atoms(['H','H','H'], positions=positions, cell=cell, pbc=pbc)
+    su1 = Atoms(['C','H','H'], positions=positions, cell=cell, pbc=pbc)
+    su2 = Atoms(['H','He','H'], positions=positions, cell=cell, pbc=pbc)
+    su3 = Atoms(['H','N','H'], positions=positions, cell=cell, pbc=pbc)
+
+    plat = ParentLattice(pri,substitutions=[su1,su2,su3],pbc=pbc)
+    #cpool = ClustersPool(plat, npoints=[0,1,2,3,4], radii=[0,0,2.3,1.42,1.42])
+    cpool = ClustersPool(plat, npoints=[1,2,3,4], radii=[0,2.3,1.42,1.42])
+    cpool.write_clusters_db(cpool.get_cpool(),cpool.get_cpool_scell(),"cpool.json")
+    corrcal = CorrelationsCalculator("trigonometric", plat, cpool)
+
+    scell = SuperCell(plat,np.array([(1,0,0),(0,3,0),(0,0,1)]))
+    strset = StructuresSet(plat, filename="test_cluster_selector_structures_set.json")
+    nstr = 20
+    #for i in range(nstr):
+    #    strset.add_structure(scell.gen_random(nsubs={}))
+    strset.add_structure(Structure(scell,[1,2,1,6,7,1,1,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,1,1,1,1,1,1,1,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,2,1,1,7,1,6,7,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,1,1,6,1,1,1,7,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,7,1,6,7,1,6,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,7,1,6,2,1,1,1,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,1,1,1,1,1,1,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,1,1,1,7,1,6,7,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,2,1,6,2,1,6,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,1,1,6,2,1,1,1,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,1,1,6,7,1,1,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,2,1,6,2,1,1,1,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,7,1,1,1,1,6,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,1,1,1,7,1,1,7,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,2,1,6,7,1,6,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,7,1,1,2,1,1,1,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,7,1,1,7,1,1,1,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,2,1,6,1,1,6,7,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[1,7,1,1,1,1,6,2,1]),write_to_db=True)
+    strset.add_structure(Structure(scell,[6,1,1,1,7,1,1,1,1]),write_to_db=True)
+
+    # Get the DATA(comat) + TARGET(energies)
+    comat = corrcal.get_correlation_matrix(strset)
+    strset.set_calculator(EMT2())
+    energies = strset.calculate_property()
+
+    #fitter_model = Fitter(method = "skl_LinearRegression")
+
+
+
+    clsel = ClustersSelector('size', cpool, fitter_size = "linreg2", alpha0 = 2.3)
+    clsel.select_clusters(comat,energies)
+
+    clsets.get_subpool(clsel.get_optimal_clusters())
+
+
+
+    #print(repr(cv))
+    #print(repr(rmse))
+    #print(repr(ranks))
+    #print(repr(sizes))
+
+    rcv = np.array([21.25070484696078, 21.192827866028548, 18.253021305083276, 18.25302130508316, 18.253021305083077, 26.826078769571485, 29.985365890016077, 33.07860588175105, 46.94157574864739, 49.37973761687287, 52.263685892043306])
+    rrmse = np.array([16.901897707237335, 6.70078970932807, 3.999376257086459, 3.9993762570864626, 3.999376257086468, 4.388974798433372e-13, 1.6733997988876832e-13, 4.325945001471906e-13, 1.8694837133527457e-13, 2.5232271964659314e-13, 4.896588122807044e-13])
+    rranks = np.array([3, 9, 11, 11, 11, 16, 16, 16, 16, 16, 16])
+    rsizes = np.array([3, 9, 11, 17, 21, 17, 23, 27, 20, 26, 30])
+
+    isok = isclose(cv,rcv) and isclose(rrmse, rmse) and (rranks == ranks).all() and (rsizes == sizes).all()
+    assert(isok)
