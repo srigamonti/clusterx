c3c80601f97479b3e350e73cf74404c67f0b15c2
Maria Troppenz <mariat@physik.hu-berlin.de>
2018-08-23 15:20:56 +0200

    start implementing metropolis test

---
+++ clusterx/test/test_metropolis.py
@@ -0,0 +1,90 @@
+import clusterx as c
+import subprocess
+from ase.spacegroup import crystal
+from clusterx.parent_lattice import ParentLattice
+from clusterx.super_cell import SuperCell
+from clusterx.clusters.clusters_pool import ClustersPool
+from clusterx.clusters.cluster import Cluster
+from clusterx.correlations import CorrelationsCalculator
+from ase.data import atomic_numbers as cn
+from ase import Atoms
+
+def test_clathrate_mc():
+
+    subprocess.call(["rm","-f","test_clathrate_mc-cluster_orbit.json"])
+    subprocess.call(["rm","-f","test_clathrate_mc-cpool.json"])
+
+    a = 10.5148
+    x = 0.185; y = 0.304; z = 0.116
+    wyckoff = [
+        (0, y, z), #24k
+        (x, x, x), #16i
+        (1/4., 0, 1/2.), #6c
+        (1/4., 1/2., 0), #6d
+        (0, 0 , 0) #2a
+    ]
+
+    # Build the parent lattice
+    pri = crystal(['Si','Si','Si','Ba','Ba'], wyckoff, spacegroup=223, cellpar=[a, a, a, 90, 90, 90])
+    sub = crystal(['Al','Al','Al','Ba','Ba'], wyckoff, spacegroup=223, cellpar=[a, a, a, 90, 90, 90])
+    plat = ParentLattice(atoms=pri,substitutions=[sub],pbc=(1,1,1))
+
+    # Build clusters pool
+    cpool = ClustersPool(plat)
+    cpsc = cpool.get_cpool_scell()
+    s = cn["Al"]
+    cpool.add_cluster(Cluster([],[],cpsc))
+    cpool.add_cluster(Cluster([0],[s],cpsc))
+    cpool.add_cluster(Cluster([24],[s],cpsc))
+    cpool.add_cluster(Cluster([40],[s],cpsc))
+    cpool.add_cluster(Cluster([6,4],[s,s],cpsc))
+    cpool.add_cluster(Cluster([37,32],[s,s],cpsc))
+    cpool.add_cluster(Cluster([39,12],[s,s],cpsc))
+    cpool.add_cluster(Cluster([16,43],[s,s],cpsc))
+    cpool.add_cluster(Cluster([35,11],[s,s],cpsc))
+    cpool.add_cluster(Cluster([39,30],[s,s],cpsc))
+    cpool.add_cluster(Cluster([22,17],[s,s],cpsc))
+    cpool.add_cluster(Cluster([35,42],[s,s],cpsc))
+    cpool.add_cluster(Cluster([32,14],[s,s],cpsc))
+    cpool.add_cluster(Cluster([11,10],[s,s],cpsc))
+    cpool.add_cluster(Cluster([18,9],[s,s],cpsc))
+    cpool.add_cluster(Cluster([18,43],[s,s],cpsc))
+
+    # Energy
+    cpoolE = cpool.get_subpool([0,1,2,3,4,5,6,7,9,15])
+    ecisE = [
+        -78407.3247588,
+        47.164484875,
+        47.1673476881,
+        47.1569012692,
+        0.00851281608144,
+        0.0139835351147,
+        0.0108175321899,
+        0.0101521144776,
+        0.00121744613474,
+        0.000413664306204
+    ]
+
+    mult=cpoolE.get_multiplicities()
+    print(mult)
+    multE = [1,24,16,6,12,8,48,24,24,24]
+    print(multE)
+    corcE = CorrelationsCalculator("binary-linear",plat,cpoolE)
+    scellE = SuperCell(plat,[(1,0,0),(0,1,0),(0,0,1)])
+
+    struc = scellE.gen_random({0:[16]})
+
+    nmc = 10
+
+    for i in range(nmc):
+        struc = scellE.gen_random({0:[16]})
+        #struc = struc.swap_random_binary(0)
+        corrs = corcE.get_cluster_correlations(struc,mc=True)
+        print(corrs)
+        erg = 0
+        for j in range(len(ecisE)):
+            erg += multE[j] * ecisE[j] * corrs[j]
+
+        print(i,erg)
+
+    cpool.write_clusters_db(cpool.get_cpool(),cpool.get_cpool_scell(),"test_clathrate_mc-cpool.json")
