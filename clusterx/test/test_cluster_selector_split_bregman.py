13664567ee6abcba3603d2d443835d46dffa0338
Christopher Sutton <sutton@fhi-berlin.mpg.de>
2018-09-17 13:34:20 +0200

    updated split bregman

--- clusterx/test/test_cluster_selector_split_bregman.py
+++ clusterx/test/test_cluster_selector_split_bregman.py
@@ -16,6 +16,37 @@ from clusterx.utils import isclose
 import numpy as np
 
 def test_cluster_selector_split_bregman():
+    
+    strset, energies, comat, clmults = get_structure_set()
+
+    lamb = 0.9
+    mu_min = 0.00001
+    mu_max = 0.10
+    mu_step = 0.001
+    
+    ####### Class ClusterSelector ##########
+    clsel = ClustersSelector('split_bregman', cpool, sparsity_max=mu_max, sparsity_min=mu_min, sparsity_step=mu_step, l=lamb, LOO_idx=18)
+    clsel.select_clusters(comat, energies, mult = clmults)
+    ##################################
+    cp2 = clsel.optimal_clusters._cpool
+    npoints=[]
+    radius=[]
+    for i,c in enumerate(cp2):
+        npoints.append(c.npoints)
+        radius.append(c.radius)
+
+    precomp_npoints = [0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4]
+    precomp_radius = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.4142135623730951, 1.4142135623730951, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 1.4142135623730951, 1.4142135623730951]
+
+    print ("========Asserts========")
+    isok1 = isclose(npoints, precomp_npoints)
+    isok2 = isclose(radius, precomp_radius)
+    assert(isok1)
+    assert(isok2)
+    print("\n Test of split_bregman in CELL was successful.\n\n")
+
+
+def get_structure_set():
 
     cell = [[3,0,0],
             [0,1,0],
@@ -63,38 +94,105 @@ def test_cluster_selector_split_bregman():
     strset.add_structure(Structure(scell,[6,2,1,6,1,1,6,7,1]),write_to_db=True)
     strset.add_structure(Structure(scell,[1,7,1,1,1,1,6,2,1]),write_to_db=True)
     strset.add_structure(Structure(scell,[6,1,1,1,7,1,1,1,1]),write_to_db=True)
-
+    
     # Get the DATA(comat) + TARGET(energies)
     comat = corrcal.get_correlation_matrix(strset)
 
     strset.set_calculator(EMT2())
     energies = strset.calculate_property()
+    print(energies)
+    import os, sys
+    sys.exit()
+
     clmults = cpool.get_multiplicities()
-    lamb = 0.9
-    mu_min = 0.00001
-    mu_max = 0.10
-    mu_step = 0.001
+
+    return strset, energies, comat, clmults 
+
+def build_lattice_and_get_corr():
+
+    a = 10.5148
+    x = 0.185; y = 0.304; z = 0.116
+    wyckoff = [
+        (0, y, z), #24k
+        (x, x, x), #16i
+        (1/4., 0, 1/2.), #6c
+        (1/4., 1/2., 0), #6d
+        (0, 0 , 0) #2a
+    ]
+
+    # Build the parent lattice
+    pri = crystal(['Si','Si','Si','Ba','Ba'], wyckoff, spacegroup=223, cellpar=[a, a, a, 90, 90, 90])
+    sub = crystal(['Al','Al','Al','Ba','Ba'], wyckoff, spacegroup=223, cellpar=[a, a, a, 90, 90, 90])
+    plat = ParentLattice(atoms=pri,substitutions=[sub])
+
+    scellE = SuperCell(plat,[(1,0,0),(0,1,0),(0,0,1)])
     
-    ####### Class ClusterSelector ##########
-    clsel = ClustersSelector('split_bregman', cpool, sparsity_max=mu_max, sparsity_min=mu_min, sparsity_step=mu_step, l=lamb, LOO_idx=18)
-    clsel.select_clusters(comat, energies, mult = clmults)
-    ##################################
-    cp2 = clsel.optimal_clusters._cpool
-    npoints=[]
-    radius=[]
-    for i,c in enumerate(cp2):
-        npoints.append(c.npoints)
-        radius.append(c.radius)
+    strset = StructuresSet(plat)
+    #nstr = 20
+    #for i in range(nstr):
+    #    strset.add_structure(scell.gen_random(nsubs={}))
 
-    precomp_npoints = [0, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4]
-    precomp_radius = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.4142135623730951, 1.4142135623730951, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 2.23606797749979, 1.4142135623730951, 1.4142135623730951]
+    # Build clusters pool
+    cpool = ClustersPool(plat)
+    cpsc = cpool.get_cpool_scell()
+    s = cn["Al"]
+    cpool.add_cluster(Cluster([],[],cpsc))
+    cpool.add_cluster(Cluster([0],[s],cpsc))
+    cpool.add_cluster(Cluster([24],[s],cpsc))
+    cpool.add_cluster(Cluster([40],[s],cpsc))
+    cpool.add_cluster(Cluster([6,4],[s,s],cpsc))
+    cpool.add_cluster(Cluster([37,32],[s,s],cpsc))
+    cpool.add_cluster(Cluster([39,12],[s,s],cpsc))
+    cpool.add_cluster(Cluster([16,43],[s,s],cpsc))
+    cpool.add_cluster(Cluster([35,11],[s,s],cpsc))
+    cpool.add_cluster(Cluster([39,30],[s,s],cpsc))
+    cpool.add_cluster(Cluster([22,17],[s,s],cpsc))
+    cpool.add_cluster(Cluster([35,42],[s,s],cpsc))
+    cpool.add_cluster(Cluster([32,14],[s,s],cpsc))
+    cpool.add_cluster(Cluster([11,10],[s,s],cpsc))
+    cpool.add_cluster(Cluster([18,9],[s,s],cpsc))
+    cpool.add_cluster(Cluster([18,43],[s,s],cpsc))
+    #for cl in cpool._cpool:
+    #    print("cl_idxs: ",cl.get_idxs())
 
-    print ("========Asserts========")
-    isok1 = isclose(npoints, precomp_npoints)
-    isok2 = isclose(radius, precomp_radius)
-    assert(isok1)
-    assert(isok2)
-    print("\n Test of split_bregman in CELL was successful.\n\n")
+    clarray=cpool.get_cpool_arrays()
+
+    # Energy
+    cpoolE = cpool.get_subpool([0,1,2,3,4,5,6,7,9,15])
+    ecisE = [
+        -78407.3247588,
+        47.164484875,
+        47.1673476881,
+        47.1569012692,
+        0.00851281608144,
+        0.0139835351147,
+        0.0108175321899,
+        0.0101521144776,
+        0.00121744613474,
+        0.000413664306204
+    ]
+    multE = [1,24,16,6,12,8,48,24,24,24]
+    corcE = CorrelationsCalculator("binary-linear",plat,cpoolE)
+    scellE = SuperCell(plat,[(2,0,0),(0,2,0),(0,0,2)])
+    energies = []
+    nmc = 10
+    for i in range(nmc):
+        struc = scellE.gen_random({0:[16]})
+        strset.add_structure(struc)
+        corrs = corcE.get_cluster_correlations(struc,mc=True)
+        print(corrs)
+        erg = 0
+        for j in range(len(ecisE)):
+            erg += multE[j] * ecisE[j] * corrs[j]
+        energies.append( erg )
+        print(i,erg)
+
+    print(energies)
+    corrcal = CorrelationsCalculator("binary-linear",plat,cpool)
+    comat = corrcal.get_correlation_matrix(strset)
+
+    return strset, energies, comat, clmults
+
 
 if __name__ == "__main__":
     test_cluster_selector_split_bregman()
