7d92fcd8b0474bee7520d75d7ef013beac4e425f
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2019-02-15 17:57:40 +0100

    Add license boilerplate

---
+++ clusterx/cli/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2015-2019, CELL Developers.
+# This work is licensed under the terms of the Apache 2.0 license
+# See accompanying license for details or visit https://www.apache.org/licenses/LICENSE-2.0.txt.
+
