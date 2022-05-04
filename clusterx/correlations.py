e9a31eacee9d63ccbfbe3b58fc2d072ec87901bd
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2022-05-04 13:27:49 +0200

    Remove fix since it would only work for an indicator function basis.

--- clusterx/correlations.py
+++ clusterx/correlations.py
@@ -452,9 +452,6 @@ class CorrelationsCalculator():
                 correlations[icl] += weight * cf
 
             if multiplicities is None:
-                if np.sum(weights) == 0:
-                    print(f"0 weight for cluster nr. {icl}, which is {cluster}, with weights: {weights}")
-                    continue # in this case, the correlations will be 0 - the weights were empty in our specific example
                 correlations[icl] /= np.sum(weights)
             else:
                 correlations[icl] /= multiplicities[icl]
