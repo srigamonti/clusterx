e5ad3ff9eb2bebf2a9330e4bd1ad018fc29f2a73
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2023-12-07 14:34:41 +0100

    chore: update version number and dep

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='clusterX',
-      version='2.1.0.rc2',
+      version='2.1.0.rc3',
       description='CELL (aka clusterX) is a python package for building Cluster Expansion models of simple and complex alloys and performing thermodynamical analyses of materials.',
       url='http://sol.physik.hu-berlin.de/cell/',
       author='CELL Developers',
@@ -9,5 +9,6 @@ setup(name='clusterX',
       license='http://www.apache.org/licenses/LICENSE-2.0',
       packages=find_packages(),
       install_requires=['numpy', 'scipy', 'scikit-learn', 'matplotlib','ase','plac','pytest','spglib','sympy','nglview','ipywidgets','pytest-html', 'tqdm'],
-      entry_points={'console_scripts': ['cell=clusterx.cli.main:main']}
+      entry_points={'console_scripts': ['cell=clusterx.cli.main:main']},
+      python_requires="<=3.11, >=3.0"
 )
