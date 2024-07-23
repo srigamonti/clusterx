5dd7e508d1b515d29b1585d0d2a8f8a3b19cb220
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2024-07-23 17:19:58 +0200

    feat(cli): make build_cpool cmd accept parent lattice file.

--- clusterx/cli/build_cpool.py
+++ clusterx/cli/build_cpool.py
@@ -3,19 +3,21 @@
 # See accompanying license for details or visit https://www.apache.org/licenses/LICENSE-2.0.txt.
 
 from typing import List, Tuple, Union, Optional
-import inspect
 import plac
 from clusterx.structures_set import StructuresSet
+from clusterx.parent_lattice import ParentLattice
 from clusterx.super_cell import SuperCell
 from clusterx.clusters.clusters_pool import ClustersPool
 from clusterx.cli.config_utils import convert_to_float_list, convert_to_int_list
+from clusterx.cli.config_utils import cmd_message
 
 Int2DArray = List[List[int]]
 Int3Vector = Tuple[int, int, int]
+Int3Array = List[int]
 Int2x2Matrix = List[List[int]]
 Int3x3Matrix = List[List[int]]
 # Define the alias for the psc type
-PscType = Union[int, Int2DArray, Int3Vector, Int2x2Matrix, Int3x3Matrix]
+PscType = Union[str, int, Int2DArray, Int3Vector, Int3Array, Int2x2Matrix, Int3x3Matrix]
 
 commands = ["build_cpool"]
 
@@ -24,10 +26,17 @@ commands = ["build_cpool"]
 @plac.pos("radii", help="Corresponting atom radii.")
 @plac.opt(
     "sset_filepath",
+    abbrev="ssf",
     help="You can indicate a serialized structures set json file to get the parent lattice from it.",
 )
 @plac.opt(
+    "plat_filepath",
+    abbrev="plf",
+    help="You can indicate a serialized parent lattice json file to get the parent lattice from it.",
+)
+@plac.opt(
     "psc",
+    abbrev="p",
     help=(
         "Definition of a super cell where to find clusters, in terms of the parent lattice. "
         "For a precise definition look into the parameter p of the SuperCell class of CELL."
@@ -45,6 +54,7 @@ def build_cpool(
     npoints: Union[List[int], str],
     radii: Union[List[float], str],
     sset_filepath: Optional[str] = None,
+    plat_filepath: Optional[str] = None,
     psc: PscType = 1,
     method: int = 1,
     cpool_filepath: str = "cpool.json",
@@ -57,21 +67,20 @@ def build_cpool(
     if isinstance(radii, str):
         radii = convert_to_float_list(radii)
 
-    command_name = inspect.stack()[0].function
-    config = {k: v for k, v in locals().items() if k != "command_name"}
-
-    print(f"Running {command_name} with configuration:")
-    print(config)
+    cmd_message("head")
 
     if sset_filepath is not None:
         sset = StructuresSet(filepath=sset_filepath)
         plat = sset.get_parent_lattice()
 
+    if plat_filepath is not None:
+        plat = ParentLattice(filepath=plat_filepath)
+
     scell = SuperCell(plat, p=psc)
     cpool = ClustersPool(
         plat, npoints=npoints, radii=radii, super_cell=scell, method=method
     )
-    cpool.display_info()
+    cpool.print_info()
     cpool.serialize(
         filepath=cpool_filepath, vacancy_atomic_number=vacancy_atomic_number
     )
