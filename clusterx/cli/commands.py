95aa6189eeceb216a6b2b551837a4c2eecc42bb3
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2024-08-27 16:04:28 +0200

    chore(cli): reformat commands module with linting

--- clusterx/cli/commands.py
+++ clusterx/cli/commands.py
@@ -13,45 +13,66 @@ import glob
 # in clusterx/cli/, i.e. it performs dynamical imports like:
 #
 # from clusterx.cli.plot_clusters import plot_clusters1
-# 
+#
 # as needed by plac
 ##########################################
 
 # Get list of modules
 
-mods = glob.glob(dirname(__file__)+"/*.py")
+mods = glob.glob(dirname(__file__) + "/*.py")
 
-modules = [ basename(f)[:-3] for f in mods if isfile(f) and not f.endswith('__init__.py') and not f.endswith('main.py') and not f.endswith('commands.py') and not f.endswith('config_utils.py')]
+modules = [
+    basename(f)[:-3]
+    for f in mods
+    if isfile(f)
+    and not f.endswith("__init__.py")
+    and not f.endswith("main.py")
+    and not f.endswith("commands.py")
+    and not f.endswith("config_utils.py")
+]
 
 # Check for cellinput.toml in the current working directory
-toml_file_path = os.path.join(os.getcwd(), 'cellinput.toml')
+toml_file_path = os.path.join(os.getcwd(), "cellinput.toml")
 custom_dir = os.getcwd()  # Default to CWD
 custom_modules = []
 
 if os.path.isfile(toml_file_path):
     config_dict = toml.load(toml_file_path)
-    custom_modules = config_dict.get('custom_modules', [])
-    custom_dir = config_dict.get('custom_dir', os.getcwd())
+    custom_modules = config_dict.get("custom_modules", [])
+    custom_dir = config_dict.get("custom_dir", os.getcwd())
     sys.path.append(custom_dir)
 
-# Dynamically retrieve functions from modules in clusterx.cli, 
+# Dynamically retrieve functions from modules in clusterx.cli,
 # and re-create them in the current module
-commands = [] # This array is needed by plac
+commands = []  # This array is needed by plac
 for module in modules:
-    for command in getattr(__import__('clusterx.cli.'+module,fromlist=[module]),'commands'):
+    for command in getattr(
+        __import__("clusterx.cli." + module, fromlist=[module]), "commands"
+    ):
         commands.append(command)
-        setattr(sys.modules[__name__], command, getattr(__import__('clusterx.cli.'+module,fromlist=[module]),command))
+        setattr(
+            sys.modules[__name__],
+            command,
+            getattr(__import__("clusterx.cli." + module, fromlist=[module]), command),
+        )
 
 
 # Add custom modules
 for module in custom_modules:
     module_name = module[:-3]  # Strip .py extension
-    custom_commands = getattr(__import__(module_name, fromlist=[module_name]), 'commands', [])
+    custom_commands = getattr(
+        __import__(module_name, fromlist=[module_name]), "commands", []
+    )
     for command in custom_commands:
         commands.append(command)
-        setattr(sys.modules[__name__], command, getattr(__import__(module_name, fromlist=[module_name]), command))
+        setattr(
+            sys.modules[__name__],
+            command,
+            getattr(__import__(module_name, fromlist=[module_name]), command),
+        )
 
 ##########################################
+
 
 def __init__():
     """
@@ -59,10 +80,12 @@ def __init__():
     """
     print(__init__.__doc__)
 
+
 def __missing__(name):
-    return ('Command %r does not exist' % name,)
+    return ("Command %r does not exist" % name,)
+
 
 def __exit__(etype, exc, tb):
     "Will be called automatically at the end of the intepreter loop"
-    if etype in (None, GeneratorExit): # success
-        print('ok')
+    if etype in (None, GeneratorExit):  # success
+        print("ok")
