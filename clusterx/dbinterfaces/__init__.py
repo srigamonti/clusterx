60d635cae9cb3c276cfdde02e2d786b12c412b7d
Noa_h Dasch <daschnoa_h@physik.hu-berlin.de>
2025-01-24 13:09:23 +0100

    fix(dbinterfaces): Changed default URL to NOMAD instead of SOL; corrected docstrings, changed token default to None, added comments for clarification

--- clusterx/dbinterfaces/__init__.py
+++ clusterx/dbinterfaces/__init__.py
@@ -1,4 +1,6 @@
 """"
-This module is designed to obtain data from a NOMAD or a 
-NOMAD OASIS database and make it usable for CELL.
+This module is designed to extract data from NOMAD or
+a NOMAD OASIS database (such as total values, structures, etc.)
+and use this data to generate structure sets along with their
+associated properties in CELL, which can then be used to create models.
 """
