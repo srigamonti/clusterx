56d2c09dfdd69993fa636ba6c71e6b791ee9668a
Noa_h Dasch <daschnoa_h@physik.hu-berlin.de>
2025-02-19 18:01:07 +0100

    fix(dbinterfaces): Corrected installation requirements and typo

--- clusterx/dbinterfaces/__init__.py
+++ clusterx/dbinterfaces/__init__.py
@@ -1,12 +1,12 @@
 """"
 This module is designed to extract data from NOMAD or
-a NOMAD OASIS database (such as total values, structures, etc.)
-and use this data to generate structure sets along with their
-associated properties in CELL, which can then be used to create models.
+a NOMAD OASIS database (such as total energy values, structures, etc.).
+The data is then used to generate structure sets along with their
+associated properties in CELL to build models.
 
 **Requirements:**
-    - Installation of CELL
-    Only if you want to make use of get_nomad_oasis_access_token:
+    Only if you want to make use of get_nomad_oasis_access_token with 
+    environment variables:
     - Installation of dotenv ($pip install python-dotenv)
     Alternatively you can create the token using
     'https://nomad-lab.eu/prod/v1/api/v1/extensions/docs#/auth/get_token_via_query_auth_token_get'
