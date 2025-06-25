201e1b54062412db1f6a4cf1a033069dc55521cc
Santiago Rigamonti <santiago.rigamonti@physik.hu-berlin.de>
2025-06-25 11:38:20 +0200

    refactor (test_structures_set): change some test names and make them more focused
    In particular, splitted a long old test to just check property calculation with ASE and custom calculators

--- clusterx/test/test_structures_set.py
+++ clusterx/test/test_structures_set.py
@@ -55,7 +55,7 @@ def test_set_property_values(structures_set):
     np.testing.assert_array_equal(structures_set.get_property_values("set_prop"), np.ones(len(structures_set)))
 
 
-def test_indexing(structures_set):
+def test_slicing_addition(structures_set):
     sset1 = structures_set[:5]
     sset2 = structures_set[5:]
     sset3 = sset1 + sset2
@@ -63,19 +63,23 @@ def test_indexing(structures_set):
     assert len(sset3) == len(sset1) + len(sset2)
 
 
-def test_add_sets_new(parent_lattice, super_cell):
+def test_property_transfer_addition(parent_lattice, super_cell):
     n_structures = 5
     sset1 = StructuresSet(parent_lattice)
     sset2 = StructuresSet(parent_lattice)
+    sset3 = StructuresSet(parent_lattice)
     for i in range(n_structures):
         sset1.add_structure(super_cell.gen_random_structure())
         sset2.add_structure(super_cell.gen_random_structure())
+        sset3.add_structure(super_cell.gen_random_structure())
     sset1.set_property_values(property_name="set_prop", property_vals=[1.0] * n_structures)
-    sset2.set_property_values(property_name="set_prop", property_vals=[2.0] * n_structures)
-    sset3 = sset1 + sset2
-    assert isinstance(sset3, StructuresSet)
-    assert len(sset3) == len(sset1) + len(sset2)
-    np.testing.assert_array_equal(sset3.get_property_values("set_prop"), n_structures * [1.0] + n_structures * [2.0])
+    sset3.set_property_values(property_name="set_prop", property_vals=[2.0] * n_structures)
+    sset4 = sset1 + sset2 + sset3
+    assert isinstance(sset4, StructuresSet)
+    assert len(sset4) == len(sset1) + len(sset2) + len(sset3)
+    np.testing.assert_array_equal(
+        sset4.get_property_values("set_prop"), n_structures * [1.0] + n_structures * [None] + n_structures * [2.0]
+    )
 
 
 def test_serialize_load_json(structures_set):
@@ -94,46 +98,39 @@ def test_serialize_load_sqlite(structures_set):
     np.testing.assert_array_equal(sset_loaded.get_property_values("set_prop"), [1.0] * len(structures_set))
 
 
-def test_init(parent_lattice):
-    """Test creation, union, serialization, and parsing of structures sets."""
-    cu = bulk("Cu")
-    plat = ParentLattice(atoms=cu, symbols=[["Cu", "Au"]])
-    scell1 = SuperCell(plat, 3)
+def test_property_calculation_with_ase_calculator(structures_set):
+    """Test calculation of properties with custom property solver ASE calculator"""
+    structures_set.set_calculator(EMT())
+    structures_set.compute_property_values(property_name="tote")
 
-    sset1 = StructuresSet(parent_lattice=plat)
-    np.random.seed(10)
+    tote_list = []
+    for s in structures_set:
+        ats = s.get_atoms().copy()
+        ats.calc = EMT()
+        tote_list.append(ats.get_potential_energy())
 
-    nstr1 = 10
+    np.testing.assert_array_equal(structures_set.get_property_values("tote"), tote_list)
 
-    for i in range(nstr1):
-        sset1.add_structure(scell1.gen_random_structure())
 
-    sset1.set_calculator(EMT())
-    sset1.compute_property_values(property_name="tote")
+def test_property_calculation_with_custom_solver(structures_set):
+    """Test calculation of properties with custom property solver"""
 
-    def a_prop(i, structure, **kwargs):
-        at = structure.get_atoms()
-        at.calc = EMT()
-        return at.get_potential_energy() * 0.1 - 10
+    compute_ref_value = False
 
-    sset1.compute_property_values(property_name="a_prop0", property_calc=a_prop)
-    sset1.compute_property_values(property_name="a_prop1", property_calc=a_prop)
-    sset1.set_property_values(property_name="a_prop2", property_vals=[1.0] * len(sset1))
+    def custom_prop(i, structure, **kwargs):
+        par1 = kwargs["par1"]
+        par2 = kwargs["par2"]
+        e = (i * par1 - par2) * len(structure)
+        return e
 
-    sset1.serialize(path="sset1.json", overwrite=True)
+    structures_set.compute_property_values(property_name="cprop", property_calc=custom_prop, par1=3, par2=5)
 
-    scell2 = SuperCell(plat, 2)
-    nstr2 = 5
-    sset2 = StructuresSet(parent_lattice=plat)
+    if compute_ref_value:
+        custom_prop_list = []
+        for i in range(len(structures_set)):
+            custom_prop_list.append(custom_prop(i, structures_set[i], par1=3, par2=5))
+        print(custom_prop_list)
+    else:
+        custom_prop_list = [-135, -54, 27, 108, 189, 270, 351, 432, 513, 594]
 
-    sset2.compute_property_values(property_name="a_prop1", property_calc=a_prop)
-    sset2.compute_property_values(property_name="a_prop2", property_calc=a_prop)
-
-    for i in range(nstr2):
-        sset2.add_structure(scell2.gen_random_structure())
-    sset3 = sset1 + sset2
-    assert len(sset3) == len(sset1) + len(sset2)
-    sset3.serialize(path="sset3.json", overwrite=True, rm_vac=False)
-
-    sset4 = StructuresSet(db_fname="sset3.json")
-    sset4.serialize(path="sset4.json", overwrite=True, rm_vac=False)
+    np.testing.assert_array_equal(structures_set.get_property_values("cprop"), custom_prop_list)
