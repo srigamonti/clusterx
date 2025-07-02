d87609282f84fe4d1f2db22142b818f3d5bd7e1f
Tim Bechtel <bechtelt@physik.hu-berlin.de>
2025-07-02 15:03:21 +0200

    test (dss): absorb test_hnf and test_get_unique_supercells into dss test

--- clusterx/test/test_derivative_structures.py
+++ clusterx/test/test_derivative_structures.py
@@ -2,11 +2,16 @@
 
 import pytest
 from ase import Atoms
+from ase.data import atomic_numbers as an
 import numpy as np
 import pandas as pd
 
-from clusterx.derivative_structures import DSGenerator
+from clusterx.derivative_structures import (
+    DSGenerator, get_HNFs, get_unique_supercells)
 from clusterx.parent_lattice import ParentLattice
+from clusterx.structures_set import StructuresSet
+from clusterx.super_cell import SuperCell
+from clusterx.structure import Structure
 from clusterx.calculators.emt import EMT2
 
 
@@ -195,3 +200,138 @@ def test_concentration(ds_generator_full):
     assert "frconc_binary" not in ds_generator_full.configurations.columns
     ds_generator_full.add_fractional_concentration_binary()
     assert "frconc_binary" in ds_generator_full.configurations.columns
+
+def test_get_all_hnf():
+    nrs = []
+    for n in range(1,7):
+        hnfs = get_HNFs(n)
+        nrs.append(len(hnfs))
+
+    assert (np.array(nrs) == np.array([1,7,13,35,31,91])).all()
+
+    hnfs = get_HNFs(4,pbc=(1,1,0))
+
+    hnfs_ref = [
+                [[1, 0, 0],
+                 [0, 4, 0],
+                 [0, 0, 1]],
+                [[1, 1, 0],
+                 [0, 4, 0],
+                 [0, 0, 1]],
+                [[1, 2, 0],
+                 [0, 4, 0],
+                 [0, 0, 1]],
+                [[1, 3, 0],
+                 [0, 4, 0],
+                 [0, 0, 1]],
+                [[2, 0, 0],
+                 [0, 2, 0],
+                 [0, 0, 1]],
+                [[2, 1, 0],
+                 [0, 2, 0],
+                 [0, 0, 1]],
+                [[4, 0, 0],
+                 [0, 1, 0],
+                 [0, 0, 1]]
+                ]
+
+    isok = True
+
+    # Now check that hnfs_ref is identical to hnfs
+    for m1,m2 in zip(hnfs,hnfs_ref):
+        for r1,r2 in zip(m1,m2):
+            for i1,i2 in zip(r1,r2):
+                if i1 != i2:
+                    isok = False
+                    break
+            if not isok:
+                break
+        if not isok:
+            break
+
+    assert isok
+
+
+def test_get_unique_supercells():
+    """Test generation of unique supercells.
+
+    Three cases are tested: Square 2D lattice of index 4 (the case of
+    Fig.1 of [1]); FCC lattice of index 4 (cf. the seven non-decorated
+    structures of Fig. 2 and Table IV in [2]); and the simple cubic lattice
+    (Fig.11 and Table IV of [2])
+
+    [1] Computational Materials Science 59 (2012) 101–107
+    [2] Phys. Rev. B 77, 224115 2008
+    """
+    for case in range(3):
+
+        if case == 0: #Square (2D, i.e. pbc = (1,1,0))
+            a=3.1
+            index = 4
+            cell = np.array([[1,0,0],[0,1,0],[0,0,1]])
+            positions = np.array([[0,0,0]])
+            sites = [[12,13]]
+            pris = Atoms(cell=cell*a, positions=positions*a)
+
+            pl = ParentLattice(pris, sites=sites, pbc=(1,1,0))
+
+            unique_scs, unique_trafos = get_unique_supercells(index,pl)
+
+            sset = StructuresSet(pl)
+            for t in unique_trafos:
+                scell = SuperCell(pl,t)
+                sset.add_structure(Structure(scell,scell.get_atomic_numbers()),write_to_db = True)
+
+            sset.serialize(filepath="test_get_unique_supercells-square_lattice.json", overwrite=True)
+            print("\nFound ",len(unique_scs), " unique HNFs for a 2D square lattice of index ",index)
+            #print("SCS: ", unique_scs)
+            #print("TRA: ", unique_trafos)
+            isok0 = len(unique_scs) == 4 and unique_scs[1][1][1] == 12.4
+
+        if case == 1: #FCC
+            a=3
+            index = 4
+            cell = np.array([[0.5,0.5,0.0],[0.5,0.0,0.5],[0.0,0.5,0.5]])
+            positions = np.array([[0,0,0]])
+            sites = [[an["Cu"],an["Au"]]]
+            pris_fcc = Atoms(cell=cell*a,positions=positions*a,pbc=(1,1,1))
+
+            pl = ParentLattice(pris_fcc,sites=sites)
+
+            unique_scs, unique_trafos = get_unique_supercells(index,pl)
+
+            sset = StructuresSet(pl)
+            for t in unique_trafos:
+                scell = SuperCell(pl,t)
+                sset.add_structure(Structure(scell,scell.get_atomic_numbers()),write_to_db = True)
+
+            sset.serialize(filepath="test_get_unique_supercells-fcc.json")
+            print("Found ",len(unique_scs), " unique HNFs for a FCC lattice of index ",index)
+            #print("SCS: ", unique_scs)
+            #print("TRA: ", unique_trafos)
+            isok1 = len(unique_scs) == 7 and unique_trafos[4][2][2] == 4
+
+        if case == 2: #Simple cubic
+            a=3.1
+            index = 4
+            cell = np.array([[1,0,0],[0,1,0],[0,0,1]])
+            positions = np.array([[0,0,0]])
+            sites = [[12,13]]
+            pris = Atoms(cell=cell*a, positions=positions*a)
+
+            pl = ParentLattice(pris, sites=sites, pbc=(1,1,1))
+
+            unique_scs, unique_trafos = get_unique_supercells(index,pl)
+
+            sset = StructuresSet(pl)
+            for t in unique_trafos:
+                scell = SuperCell(pl,t)
+                sset.add_structure(Structure(scell,scell.get_atomic_numbers()),write_to_db = True)
+
+            sset.serialize(filepath="test_get_unique_supercells-sc.json")
+            print("Found ",len(unique_scs), " unique HNFs for a simple cubic lattice of index ",index)
+            #print("SCS: ", unique_scs)
+            #print("TRA: ", unique_trafos)
+            isok2 = len(unique_scs) == 9 and unique_scs[4][2][2] == 12.4
+
+    assert isok0 and isok1 and isok2
