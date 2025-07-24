21dec1554a0a2f2938ceae4a8475c2d6900df2e3
Santiago Rigamonti <srigamonti@physik.hu-berlin.de>
2025-07-24 20:41:47 +0200

    chore (cli/build_ising_model): linting

--- clusterx/cli/build_ising_model.py
+++ clusterx/cli/build_ising_model.py
@@ -1,19 +1,13 @@
 # Copyright (c) 2015-2024, CELL Developers.
 # This work is licensed under the terms of the Apache 2.0 license
 # See accompanying license for details or visit https://www.apache.org/licenses/LICENSE-2.0.txt.
-import random
-from typing import Optional
 
-import numpy as np
 import plac
 from ase import Atoms
-from sklearn.linear_model import LinearRegression
 
 from clusterx.cli.config_utils import cmd_message
-from clusterx.clusters.cluster import Cluster
 from clusterx.clusters.clusters_pool import ClustersPool
 from clusterx.correlations import CorrelationsCalculator
-from clusterx.model import Model
 from clusterx.parent_lattice import ParentLattice
 from clusterx.super_cell import SuperCell
 
@@ -21,9 +15,24 @@ commands = ["build_ising_model"]
 
 
 @plac.annotations(
-    suffix=("Suffix to be added to the file names created by this command", "option", None, str),
-    prefix=("Prefix to be added to the file names created by this command", "option", None, str),
-    true3d=("By default 2D Ising model is built. Put this to true for 3D Ising model", "flag", "t3d", bool),
+    suffix=(
+        "Suffix to be added to the file names created by this command",
+        "option",
+        None,
+        str,
+    ),
+    prefix=(
+        "Prefix to be added to the file names created by this command",
+        "option",
+        None,
+        str,
+    ),
+    true3d=(
+        "By default 2D Ising model is built. Put this to true for 3D Ising model",
+        "flag",
+        "t3d",
+        bool,
+    ),
 )
 def build_ising_model(suffix: str = "", prefix: str = "", true3d: bool = False):
     """Build Ising model. Used for demonstration purposes"""
@@ -50,7 +59,9 @@ def build_ising_model(suffix: str = "", prefix: str = "", true3d: bool = False):
     scell = SuperCell(plat, p=2)
     scell.serialize(filepath=f"{prefix}scell-ising-3d{suffix}.json")
 
-    cpool = ClustersPool(parent_lattice=plat, npoints=[2], radii=[1.1], super_cell=scell)
+    cpool = ClustersPool(
+        parent_lattice=plat, npoints=[2], radii=[1.1], super_cell=scell
+    )
     cpool.gen_clusters()
 
     if not true3d:
@@ -58,7 +69,9 @@ def build_ising_model(suffix: str = "", prefix: str = "", true3d: bool = False):
     else:
         cpool.serialize(filepath=f"{prefix}cpool-ising-3d{suffix}.json")
 
-    ccalc = CorrelationsCalculator(basis="indicator-binary", parent_lattice=plat, clusters_pool=cpool)
+    ccalc = CorrelationsCalculator(
+        basis="chebyshev", parent_lattice=plat, clusters_pool=cpool
+    )
 
     """
     scell = SuperCell(plat, p=2)
