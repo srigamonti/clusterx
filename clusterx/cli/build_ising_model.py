991e991454adc3e5a9160ab9bc85627e7e82497e
Santiago Rigamonti <santiago.rigamonti@physik.hu-berlin.de>
2025-07-25 16:47:06 +0200

    fix (cli/build_ising_model): sklearn expects coef being np array. Remove 3d option. simplify cpool creation

--- clusterx/cli/build_ising_model.py
+++ clusterx/cli/build_ising_model.py
@@ -2,6 +2,7 @@
 # This work is licensed under the terms of the Apache 2.0 license
 # See accompanying license for details or visit https://www.apache.org/licenses/LICENSE-2.0.txt.
 
+import numpy as np
 import plac
 from ase import Atoms
 from sklearn.linear_model import LinearRegression
@@ -9,9 +10,9 @@ from sklearn.linear_model import LinearRegression
 from clusterx.cli.config_utils import cmd_message
 from clusterx.clusters.clusters_pool import ClustersPool
 from clusterx.correlations import CorrelationsCalculator
+from clusterx.model import Model
 from clusterx.parent_lattice import ParentLattice
 from clusterx.super_cell import SuperCell
-from clusterx.model import Model
 
 commands = ["build_ising_model"]
 
@@ -32,12 +33,6 @@ commands = ["build_ising_model"]
         None,
         str,
     ),
-    true3d=(
-        "By default 2D Ising model is built. Put this to true for 3D Ising model",
-        "flag",
-        "t3d",
-        bool,
-    ),
 )
 def build_ising_model(
     e0: float = 0.0,
@@ -45,55 +40,34 @@ def build_ising_model(
     j1: float = 1.0,
     suffix: str = "",
     prefix: str = "",
-    true3d: bool = False,
 ):
-    """Build Ising model. Used for demonstration purposes"""
+    """Build 2D-Ising model. Used for demonstration purposes"""
     cmd_message("head")
 
     a = 1.0
     positions = [(0, 0, 0)]
     cell = [(a, 0, 0), (0, a, 0), (0, 0, a)]
-    if not true3d:
-        pbc = (True, True, False)
-    else:
-        raise NotImplementedError("3D Ising is not implemented yet.")
+    pbc = (True, True, False)
 
     pri = Atoms("H", positions=positions, cell=cell, pbc=pbc)
     sub = Atoms("He", positions=positions, cell=cell, pbc=pbc)
 
     plat = ParentLattice(pri, substitutions=[sub], pbc=pbc)
 
-    if not true3d:
-        plat.serialize(filepath=f"{prefix}plat-ising-2d{suffix}.json")
-    else:
-        plat.serialize(filepath=f"{prefix}plat-ising-3d{suffix}.json")
+    plat.serialize(filepath=f"{prefix}plat-ising{suffix}.json")
 
-    scell = SuperCell(plat, p=2)
-    scell.serialize(filepath=f"{prefix}scell-ising-3d{suffix}.json")
+    cpool = ClustersPool(parent_lattice=plat, npoints=[1, 2], radii=[0, 1.1])
 
-    cpool = ClustersPool(
-        parent_lattice=plat, npoints=[1, 2], radii=[0, 1.1], super_cell=scell
-    )
-    cpool.gen_clusters()
+    cpool.serialize(filepath=f"{prefix}cpool-ising{suffix}.json")
 
-    if not true3d:
-        cpool.serialize(filepath=f"{prefix}cpool-ising-2d{suffix}.json")
-    else:
-        cpool.serialize(filepath=f"{prefix}cpool-ising-3d{suffix}.json")
-
-    ccalc = CorrelationsCalculator(
-        basis_name="chebyshev", parent_lattice=plat, clusters_pool=cpool
-    )
+    ccalc = CorrelationsCalculator(basis_name="chebyshev", parent_lattice=plat, clusters_pool=cpool)
 
     estimator = LinearRegression()
     n_features = len(cpool)
 
-    estimator.coef_ = [-h0, -j1]
+    estimator.coef_ = np.array([-h0, -j1])
     estimator.intercept_ = e0
 
     cemodel = Model(corrc=ccalc, property_name="Energy", estimator=estimator)
 
-    if not true3d:
-        cemodel.serialize(filepath=f"{prefix}ising_model_2d{suffix}.pickle")
-    else:
-        cemodel.serialize(filepath=f"{prefix}ising_model_3d{suffix}.pickle")
+    cemodel.serialize(filepath=f"{prefix}ising-model{suffix}.pickle")
