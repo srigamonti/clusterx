57b81c90ef994498f241e332bb0773800f401cdc
Santiago Rigamonti <santiago.rigamonti@icloud.com>
2025-08-08 02:52:04 +0200

    perf (super_cell): faster generation of random structure

--- clusterx/super_cell.py
+++ clusterx/super_cell.py
@@ -454,9 +454,7 @@ class SuperCell(ParentLattice):
         _, sigmas = self.gen_random_decoration(_nsubs)
 
         return clusterx.structure.Structure(
-            SuperCell(
-                self._plat, self._p, self._sort_key, sym_table=bool(self._sym_table)
-            ),
+            self,
             sigmas=sigmas,
             mc=mc,
         )
